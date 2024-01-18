3812db630bc56bd252bb4cc26de7585a1ca6eaee
sreeram <sreeram@Sreerams-MacBook-Pro.local>
2024-01-18 19:50:45 +0530

    fix(utils): made the env check configurable

--- libs/utils/src/lib/env.validate.ts
+++ libs/utils/src/lib/env.validate.ts
@@ -17,7 +17,10 @@ type TResponse<T extends TCheckEnv> = {
     [K in keyof T]: Partial<{ [Key in UnionOfArrayValues<T[K]['optional']>]: string }> & { [Key in UnionOfArrayValues<T[K]['required']>]: string };
 };
 
-export function getEnvironmentVariables<const T extends TCheckEnv>(options: T): TResponse<T> {
+export function getEnvironmentVariables<const T extends TCheckEnv>(
+    options: T,
+    throwError = false,
+): { variables: TResponse<T>; missingMandatory: Array<string>; missingOptional: Array<string> } {
     const keys: Array<keyof T> = Object.keys(options);
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const env: any = {};
@@ -29,15 +32,18 @@ export function getEnvironmentVariables<const T extends TCheckEnv>(options: T): 
         const { optional, required } = category;
         required.forEach((variable) => {
             const value = process.env[variable];
-            if (!value) missingMandatory.push(`${String(key)}.${variable} is missing`);
+            if (!value) missingMandatory.push(`${String(key)}.${variable}`);
             else env[key][variable] = value;
         });
 
         optional.forEach((variable) => {
             const value = process.env[variable];
-            if (!value) missingOptional.push(`${String(key)}.${variable} is missing`);
+            if (!value) missingOptional.push(`${String(key)}.${variable}`);
             else env[key][variable] = value;
         });
     });
-    return deepFreeze(env);
+    if (throwError && missingMandatory.length > 0) {
+        throw new Error(`Missing environment variables [${JSON.stringify({ missingMandatory, missingOptional })}]`);
+    }
+    return { variables: deepFreeze(env), missingMandatory, missingOptional };
 }
--- packages/youreit/src/environment.ts
+++ packages/youreit/src/environment.ts
@@ -1,11 +1,14 @@
 import { getEnvironmentVariables } from '@wander-tag/utils';
 
 export const environment = {
-    properties: getEnvironmentVariables({
-        REDIS_CLUSTER: {
-            required: ['REDIS_CLUSTER_HOST', 'REDIS_CLUSTER_PORT', 'REDIS_CLUSTER_USER', 'REDIS_CLUSTER_PASS'],
-            optional: [],
+    ...getEnvironmentVariables(
+        {
+            REDIS_CLUSTER: {
+                required: ['REDIS_CLUSTER_HOST', 'REDIS_CLUSTER_PORT', 'REDIS_CLUSTER_USER', 'REDIS_CLUSTER_PASS'],
+                optional: [],
+            },
         },
-    }),
+        true,
+    ),
     production: process.env.NODE_ENV != 'development',
 } as const;
